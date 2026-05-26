8dfdbaf8a3c74fcfe8ad09df8a767790c0a175a7
heimsharon <heimsharon@hotmail.com>
2026-05-26 17:16:30 -0400

    config updates

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from "vite";
-
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    port: process.env.PORT || 3000,
+    port: Number(process.env.PORT) || 3000,
     open: true,
     proxy: {
-      "/api": {
-        target: "http://localhost:3001",
+      '/api': {
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
       },
